1ee659b3b99d1a7fecfa364b4b7b790d29624da6
kayo220 <kayo220@users.noreply.github.com>
2022-03-18 18:57:14 +0000

    deploy: d0c82ee424dd74e9c8a193566aa445dd30058bd9

---
+++ precache-manifest.5d8a3a12161fd534714623a790abbf64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a98251d891d3de1af526c14a8c0ee5d",
+    "url": "https://kayo220.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "4b5314f5f95212b60f91",
+    "url": "https://kayo220.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "4325bc3fb9b94021aef1",
+    "url": "https://kayo220.github.io/task-management-frontend/static/js/2.e8a9c5d4.chunk.js"
+  },
+  {
+    "revision": "4b5314f5f95212b60f91",
+    "url": "https://kayo220.github.io/task-management-frontend/static/js/main.cee965ff.chunk.js"
+  },
+  {
+    "revision": "5594a86eb4947f867cd1",
+    "url": "https://kayo220.github.io/task-management-frontend/static/js/runtime~main.728e876a.js"
+  }
+]);
